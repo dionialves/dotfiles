b18e60d2c99d65f989a3e746ea27965899be3263
Dioni Alves <dionialves@gmail.com>
2025-02-06 13:09:24 -0300

    New files on lazynvim

--- lua/plugins/onedark.lua
+++ lua/plugins/onedark.lua
@@ -4,9 +4,9 @@ return {
     priority = 1000, -- Faz o tema carregar antes dos outros plugins
     config = function()
       require("onedark").setup {
-        style = "warmer", -- Escolha entre dark, darker, cool, deep, warm, warmer
-        transparent = false, -- Fundo transparente se desejar
-        term_colors = true, -- Habilita cores no terminal integrado
+        style = "cool",         -- Escolha entre dark, darker, cool, deep, warm, warmer
+        transparent = false,    -- Fundo transparente se desejar
+        term_colors = true,     -- Habilita cores no terminal integrado
       }
       require("onedark").load() -- Aplica o tema
     end,
