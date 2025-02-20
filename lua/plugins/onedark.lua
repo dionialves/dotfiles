bdf77476654f1b9bd11edee9c65552c816342b32
Dioni Alves <dionialves@gmail.com>
2025-02-19 22:44:24 -0300

    Add themes

--- lua/plugins/onedark.lua
+++ lua/plugins/onedark.lua
@@ -1,14 +1,14 @@
 return {
-  {
-    "navarasu/onedark.nvim",
-    priority = 1000, -- Faz o tema carregar antes dos outros plugins
-    config = function()
-      require("onedark").setup {
-        style = "cool",         -- Escolha entre dark, darker, cool, deep, warm, warmer
-        transparent = false,    -- Fundo transparente se desejar
-        term_colors = true,     -- Habilita cores no terminal integrado
-      }
-      require("onedark").load() -- Aplica o tema
-    end,
-  },
+    {
+        "navarasu/onedark.nvim",
+        priority = 1000, -- Faz o tema carregar antes dos outros plugins
+        config = function()
+            require("onedark").setup {
+                style = "cool", -- Escolha entre dark, darker, cool, deep, warm, warmer
+                transparent = false, -- Fundo transparente se desejar
+                term_colors = true, -- Habilita cores no terminal integrado
+            }
+            --require("onedark").load() -- Aplica o tema
+        end,
+    },
 }
