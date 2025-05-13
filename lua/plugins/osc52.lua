f6bb4b403d16ea9bace5401814fa5c6eefcc4d99
Dioni Alves <dionialves@gmail.com>
2025-05-13 10:03:57 -0300

    Adicionado suporta a copia de texto

---
+++ lua/plugins/osc52.lua
@@ -0,0 +1,21 @@
+return {
+  "ojroques/nvim-osc52",
+  event = "VeryLazy",
+  config = function()
+    require("osc52").setup({
+      max_length = 0,
+      silent = false,
+      trim = false,
+    })
+
+    local function copy()
+      if vim.v.event.operator == "y" and vim.v.event.regname == "" then
+        require("osc52").copy_register("")
+      end
+    end
+
+    vim.api.nvim_create_autocmd("TextYankPost", {
+      callback = copy,
+    })
+  end,
+}
