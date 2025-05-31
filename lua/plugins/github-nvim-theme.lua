cd7e23ad83dd7b3465c67c833ff77f4d61a3e73e
Dioni Alves <dionialves@gmail.com>
2025-05-30 21:48:03 -0300

    Update themes

--- lua/plugins/github-nvim-theme.lua
+++ lua/plugins/github-nvim-theme.lua
@@ -11,7 +11,7 @@ return {
         -- ...
       })
       --vim.cmd("colorscheme github_dark")
-      vim.cmd("colorscheme github_dark_default")
+      --vim.cmd("colorscheme github_dark_default")
       --vim.cmd("colorscheme github_dark_dimmed")
     end,
   },
